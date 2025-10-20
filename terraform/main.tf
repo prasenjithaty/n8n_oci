559eec79f5b7c064a57ed550e121c5f0d21cba88
Prasenjit <prasenjit.haty@gmail.com>
2025-10-20 12:59:25 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -113,11 +113,6 @@ resource "oci_core_instance" "n8n_instance" {
   display_name        = "n8n-instance"
   shape               = "VM.Standard.E2.1.Micro"
 
-#  shape_config {
-#    memory_in_gbs = 4
-#    ocpus         = 1
-#  }
-
   source_details {
     source_type = "image"
     source_id   = data.oci_core_images.ubuntu.images[0].id
